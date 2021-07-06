61fb4978cf7c96f81b191062b6f0e9cbfd9e273d
Dmytro Kolisnyk <dmytro.kolisnyk@beetroot.se>
2021-07-06 18:16:44 +0300

    add Fleet: Form layout

--- wp-content/themes/drivewyze/template-parts/block/contact-form/block.php
+++ wp-content/themes/drivewyze/template-parts/block/contact-form/block.php
@@ -12,21 +12,28 @@
  * @var array $block
  */
 
-$slug         	= str_replace( 'acf/', '', $block['name'] );
-$block_id     	= $slug . '-' . $block['id'];
-$block_class  	= $slug . '-' . 'section';
-$form_id	  	= get_field( 'choices' );
-$section_title	= get_field( 'section_title' );
+$slug         		= str_replace( 'acf/', '', $block['name'] );
+$block_id     		= $slug . '-' . $block['id'];
+$block_class  		= $slug . '-' . 'section';
+$form_id	  		= get_field( 'choices' );
+$section_title		= get_field( 'section_title' );
+$section_subtitle	= get_field( 'section_subtitle' );
+$bg_color			= get_field( 'background_color' );
+$block_class_mod	= $bg_color ? ' ' . $block_class . '-container_white' : '';
 ?>
 <section id="<?php echo $block_id; ?>" class="<?php echo $block_class; ?>">
 
 	<?php if( $form_id || $section_title ) : ?>
-		<div class="<?php echo $block_class . '-container' ?>">
+		<div class="<?php echo $block_class . '-container' . $block_class_mod; ?>">
 
 			<div class="<?php echo $block_class . '-container__column'; ?>">
 
 				<?php if( $section_title ) : ?>
 					<h2><?php echo $section_title; ?></h2>
+				<?php endif; ?>
+
+				<?php if( $bg_color ) : ?>
+					<p><?php echo $section_subtitle; ?></p>
 				<?php endif; ?>
 
 			</div>
