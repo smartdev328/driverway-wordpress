4c0fe242fc7d96b310ae2b32a0803381be50ca96
Yuliia <yuliia.lunova@beetroot.se>
2021-07-14 13:58:59 +0300

    add post single

---
+++ wp-content/themes/drivewyze/template-parts/content-post.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Template part for displaying posts.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ * @package Drivewyze
+ */
+
+$prefix   = get_field( 'page_prefix', 'options' );
+$category = get_the_category()[0]->name;
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<div class="post-header">
+		<div class="post-header__container">
+			<div class="post-header__top">
+				<?php
+				if ( function_exists('yoast_breadcrumb') ) :
+					yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
+				endif; ?>
+
+				<?php if ( $category ) : ?>
+					<p class="post-category">
+						<?php echo $prefix ? ''. $prefix .' /' : '' ?>
+						<?php echo $category; ?>
+					</p>
+				<?php endif; ?>
+
+				<?php the_title( '<h1>', '</h1>' ); ?>
+			</div>
+
+			<div class="post-header__bottom">
+				<p class="post-date">
+					<?php echo get_the_date() ?>
+				</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="post-main">
+		<?php the_content(); ?>
+	</div>
+</article>
