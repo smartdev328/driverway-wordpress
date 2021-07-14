6a08fc58044066f30deba3a1c949de0a5ad7d436
Vlad Kushnir <vlad.kushnir@beetroot.se>
2021-07-14 10:22:48 +0300

    add two-columns-content block

--- wp-content/themes/drivewyze/functions.php
+++ wp-content/themes/drivewyze/functions.php
@@ -129,3 +129,4 @@ function om_search_form( $form ) {
 add_filter( 'get_search_form', 'om_search_form' );
 
 add_image_size( 'team-image', 443, 443, true );
+add_image_size( 'two-columns', 915, 450, true );
