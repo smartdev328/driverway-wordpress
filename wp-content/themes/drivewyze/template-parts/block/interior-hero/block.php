13b50e5d9fba79c5b7e95591267c49d2042622b4
Vlad Kushnir <vlad.kushnir@beetroot.se>
2021-07-21 16:30:06 +0300

    add interior hero block

---
+++ wp-content/themes/drivewyze/template-parts/block/interior-hero/block.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Block Name: Interior Hero
+ * Description: Interior Hero block managed with ACF.
+ * Category: common
+ * Icon: welcome-widgets-menus
+ * Keywords: interior hero acf block
+ * Supports: { "align":false, "anchor":true }
+ *
+ * @package Drivewyze
+ *
+ * @var array $block
+ */
+
+if( isset( $block['data']['preview_image_help'] )  ) :
+    echo '<img src="'. $block['data']['preview_image_help'] .'" style="width:100%; height:auto;">';
+endif;
+
+$slug       = str_replace( 'acf/', '', $block['name'] );
+$block_id   = !empty($block['anchor']) ? $block['anchor'] : $slug . '-' . $block['id'];
+$bg			= get_field( 'background_image' );
+$left_side	= get_field( 'left_side' );
+$right_side	= get_field( 'right_side' );
+?>
+<section id="<?php echo $block_id; ?>" class="<?php echo $slug; ?>">
+    <?php if( ! empty( $bg ) && is_array( $bg ) ) : ?>
+        <div class="<?php echo $slug; ?>__bg" style="background-image: url(<?php echo $bg['url'] ?>)"></div>
+    <?php endif; ?>
+
+    <?php if ( function_exists('yoast_breadcrumb') ) {
+        yoast_breadcrumb( '<p id="breadcrumbs" class="breadcrumbs">','</p>' );
+    } ?>
+
+    <div class="<?php echo $slug; ?>__block <?php echo $slug; ?>__block_image">
+        <?php if( $left_side ) : ?>
+            <div class="<?php echo $slug; ?>__left">
+                <?php if( $left_side['prefix'] ) : ?>
+                    <ul class="left-prefix">
+                        <li><?php echo $left_side['prefix'] ?></li>
+                    </ul>
+                <?php endif; ?>
+
+                <?php if( $left_side['title'] ) : ?>
+                    <h1 class="left-title"><?php echo $left_side['title'] ?></h1>
+                <?php endif; ?>
+
+                <?php if( $left_side['info'] ) : ?>
+                    <p class="left-info"><?php echo $left_side['info'] ?></p>
+                <?php endif; ?>
+
+                <?php if ( ! empty( $left_side['link'] ) && is_array( $left_side['link']) ) :
+                    $link_target = $left_side['link']['target'] ? $left_side['link']['target'] : '_blank'; ?>
+                    <a class="left-link" href="<?php echo $left_side['link']['url']; ?>"
+                       target="<?php echo esc_attr( $link_target ); ?>">
+                        <span><?php echo $left_side['link']['title']; ?></span>
+                    </a>
+                <?php endif; ?>
+            </div>
+        <?php endif; ?>
+
+        <?php if( $right_side['select_type'] == 'info' ) { ?>
+
+            <?php if( $right_side['info'] ) : ?>
+                <div class="<?php echo $slug; ?>__right <?php echo $slug; ?>__right_info">
+                    <?php echo $right_side['info'] ?>
+                </div>
+            <?php endif; ?>
+
+        <?php } else { ?>
+
+            <?php if( ! empty( $right_side['image'] ) && is_array( $right_side['image']) ) : ?>
+                <div class="<?php echo $slug; ?>__right <?php echo $slug; ?>__right_image">
+                    <img src="<?php echo $right_side['image']['url'] ?>" alt="<?php echo $right_side['image']['alt'] ?>">
+                    <?php if( $right_side['text_in_image'] ) : ?>
+                        <p><?php echo $right_side['text_in_image'] ?></p>
+                    <?php endif; ?>
+                </div>
+            <?php endif; ?>
+
+        <?php } ?>
+    </div>
+</section>
